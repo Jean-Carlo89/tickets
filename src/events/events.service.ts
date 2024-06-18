d5556509392c8a6a793ea7cd82e47eeae2e02a16
Jean Carlo <jeancarlodev@gmail.com>
2024-06-17 21:13:48 -0300

    init

---
+++ src/events/events.service.ts
@@ -0,0 +1,36 @@
+import { PrismaService } from './../prisma/prisma.service';
+import { Injectable } from '@nestjs/common';
+import { CreateEventDto } from './dto/create-event.dto';
+import { UpdateEventDto } from './dto/update-event.dto';
+
+@Injectable()
+export class EventsService {
+  constructor(private prismaService: PrismaService) {}
+
+  create(createEventDto: CreateEventDto) {
+    // this.prismaService.event.
+
+    return this.prismaService.event.create({
+      data: { ...createEventDto, date: new Date(createEventDto.date) },
+    });
+  }
+
+  findAll() {
+    return this.prismaService.event.findMany();
+  }
+
+  findOne(id: string) {
+    return this.prismaService.event.findUnique({ where: { id } });
+  }
+
+  update(id: string, updateEventDto: UpdateEventDto) {
+    return this.prismaService.event.update({
+      data: updateEventDto,
+      where: { id },
+    });
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} event`;
+  }
+}
